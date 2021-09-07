f115c7c9e5536ba87278a85cbd07996664930f2c
Joaquin Gayoso-Cabada <j.gayoso@upm.es>
2021-09-07 12:58:26 +0200

    Es aqui donde falla

--- ClavyJSONImport/src/fdi/ucm/server/importparser/json/mainProcesso_ex2_Observation.java
+++ ClavyJSONImport/src/fdi/ucm/server/importparser/json/mainProcesso_ex2_Observation.java
@@ -5,13 +5,17 @@ import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
+import fdi.ucm.server.modelComplete.collection.document.CompleteDocuments;
+import fdi.ucm.server.modelComplete.collection.document.CompleteElement;
+import fdi.ucm.server.modelComplete.collection.document.CompleteTextElement;
+
 public class mainProcesso_ex2_Observation extends CollectionJSON{
 
 	public static void main(String[] args) {
 		CollectionJSON C=new CollectionJSON();
 		ArrayList<String> log = new ArrayList<String>();
 		C.debugfile=true;
-		C.procesaJSONFolder("files/ex2", log);
+		C.procesaJSONFolder("files/ex2/Observation", log);
 		
 		 try {
 				String FileIO = System.getProperty("user.home")+File.separator+System.currentTimeMillis()+".clavy";
@@ -26,6 +30,22 @@ public class mainProcesso_ex2_Observation extends CollectionJSON{
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+		 
+		 System.out.println("Document Debug");
+		 for (CompleteDocuments documento : C.getCollection().getEstructuras()) {
+			System.out.println(documento.getDescriptionText());
+			for (CompleteElement complete_elemento : documento.getDescription()) {
+				System.out.print(complete_elemento.getHastype().getName()+"^^"+
+						complete_elemento.getHastype().toString()+"-<>-"+complete_elemento.getHastype().getClassOfIterator().toString());
+				
+				if (complete_elemento instanceof CompleteTextElement && !((CompleteTextElement) complete_elemento).getValue().trim().isEmpty())
+					System.out.print("-$$-"+((CompleteTextElement) complete_elemento).getValue());
+				
+				
+				System.out.println();
+				
+			}
+		}
 	}
 	
 }
